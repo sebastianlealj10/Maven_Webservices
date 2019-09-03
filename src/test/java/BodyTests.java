899ceb6e37da5582d37229c31eea5d09eee0f281
Sebastian Leal <sebastianlealj10@gmail.com>
2019-09-02 19:38:32 -0500

    Code cleanup

--- src/test/java/BodyTests.java
+++ src/test/java/BodyTests.java
@@ -1,25 +1,20 @@
-import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
-
-import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import io.restassured.response.Response;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import io.restassured.matcher.RestAssuredMatchers.*;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 
 public class BodyTests extends TestBase {
 
-        @Test
-            public void test_getUserBody() {
-            given().
-                    when().contentType(ContentType.JSON).
-                    get("12").
-                    then().
-                    assertThat().
-                    body("email",equalTo("sebas@gmail.com"));
+    @Test
+    public void test_getUserBody() {
+        given().
+                when().contentType(ContentType.JSON).
+                get("12").
+                then().
+                assertThat().
+                body("email", equalTo("sebas@gmail.com"));
     }
 
 
